c01d8253b7e6ffc62d58f86d9e947f471bcf6b36
lughjw <lughjw@gmail.com>
2020-02-03 21:49:28 -0600

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -23,4 +23,17 @@ class Student
         BoatingTest.new(self, boating_test_name, boating_test_status, instructor)
     end
 
+    def grade_percentage
+        my_tests = BoatingTest.all.select do |bt|
+            bt.student == self
+        end
+
+        passed_tests = my_tests.select do 
+            |bt| bt.status == 'passed'
+        end
+
+        percent = passed_tests.size.to_f / my_tests.size.to_f * 100
+        percent.round(2)
+    end
+
 end
